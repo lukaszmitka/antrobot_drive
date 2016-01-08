8db254821ad5f0c157c8428d01d1bf8b5bf0ed69
Łukasz Mitka <luk.mitka@uj.edu.pl>
2016-01-08 14:04:29 +0100

    Basic functionality

---
+++ src/antrobot_drive.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+#include "rs232.h"
+
+int send_command(std::string command, std::vector<int> *position);
+int init_comport(int port_number);
+int close_comport(int port_number);
